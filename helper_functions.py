99113d0101c5d1eb77c8b77ea43cd7814a31d090
uygarkaya <uygar.kaya@avl.com>
2023-04-09 18:58:13 +0300

    helper function is added

---
+++ helper_functions.py
@@ -0,0 +1,72 @@
+import torch
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+
+def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Tensor):
+    # Plots decision boundaries of model predicting on X in comparison to y
+    # Put everything to CPU (works better with NumPy + Matplotlib)
+    model.to("cpu")
+    X, y = X.to("cpu"), y.to("cpu")
+
+    # Setup prediction boundaries and grid
+    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+    y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 101), np.linspace(y_min, y_max, 101))
+
+    # Make features
+    X_to_pred_on = torch.from_numpy(np.column_stack((xx.ravel(), yy.ravel()))).float()
+
+    # Make predictions
+    model.eval()
+    with torch.inference_mode():
+        y_logits = model(X_to_pred_on)
+
+    # Test for multi-class or binary and adjust logits to prediction labels
+    if len(torch.unique(y)) > 2:
+        y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)  # mutli-class
+    else:
+        y_pred = torch.round(torch.sigmoid(y_logits))  # binary
+
+    # Reshape preds and plot
+    y_pred = y_pred.reshape(xx.shape).detach().numpy()
+    plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+
+def plot_predictions(train_data, train_labels, test_data, test_labels, predictions=None):
+  plt.figure(figsize=(10, 7))
+
+  # Plot training data in blue
+  plt.scatter(train_data, train_labels, c='b', label='Train Data')
+
+  # Plot testing data in green
+  plt.scatter(test_data, test_labels, c='g', label='Test Data')
+
+  # Are there predictions?
+  if predictions is not None:
+    # Plot the predictions if they exist
+    plt.scatter(test_data, predictions, c='r', label='Predictions')
+  
+  plt.legend(prop={'size': 14})
+
+
+def visualize_loss(epoch_count_list, train_loss_list, test_loss_list):
+  plt.figure(figsize=(10, 7))
+
+  # Plot training loss
+  plt.plot(epoch_count_list, train_loss_list, label='Train Loss')
+  plt.plot(epoch_count_list, test_loss_list, label='Test Loss')
+  plt.title('Train & Test Loss Curves')
+  plt.xlabel('Epoch')
+  plt.ylabel('Loss')
+  plt.legend(prop={'size': 14})
+
+
+# Evaluation Function
+def accuracy_func(y_pred, y_true):
+  correct = torch.eq(y_true, y_pred).sum().item()
+  return ((correct/len(y_true))*100)
