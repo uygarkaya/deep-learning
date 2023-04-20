956c5b346791be7823140d49b67d5660fa053b9b
Uygar Kaya <uygar.kaya@ozu.edu.tr>
2023-04-20 23:44:28 +0300

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from matplotlib import pyplot as plt
 from sklearn import metrics
+from pathlib import Path
 
 
 def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Tensor):
@@ -58,26 +59,51 @@ def plot_predictions(train_data, train_labels, test_data, test_labels, predictio
 def visualize_loss(epoch_count_list, train_loss_list, test_loss_list):
   plt.figure(figsize=(10, 7))
 
-  # Plot training loss
+  # Plot Training & Testing Loss
   plt.plot(epoch_count_list, train_loss_list, label='Train Loss')
   plt.plot(epoch_count_list, test_loss_list, label='Test Loss')
   plt.title('Train & Test Loss Curves')
   plt.xlabel('Epoch')
   plt.ylabel('Loss')
   plt.legend(prop={'size': 14})
+    
+    
+def visualize_accuracy(epoch_count_list, train_acc_list, test_acc_list):
+  plt.figure(figsize=(10, 7))
+
+  # Plot Training & Testing Accuracy
+  plt.plot(epoch_count_list, train_acc_list, label='Train Accuracy')
+  plt.plot(epoch_count_list, test_acc_list, label='Test Accuracy')
+  plt.title('Train & Test Accuracy Curves')
+  plt.xlabel('Epoch')
+  plt.ylabel('Accuracy')
+  plt.legend(prop={'size': 14})
 
 
+def save_model(model, model_name):
+  # 1. Create models directory
+  MODEL_PATH = Path('models')
+  MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+  # 2. Create model save path
+  MODEL_NAME = model_name+'.pth'
+  MODEL_SAVE_PATH = MODEL_PATH/MODEL_NAME
+
+  # 3. Save the model state dict
+  torch.save(model.state_dict(), MODEL_SAVE_PATH)
+  
+    
 # Evaluation Function
 def accuracy_func(y_pred, y_true):
-    correct = torch.eq(y_true, y_pred).sum().item()
-    return ((correct/len(y_true))*100)
+  correct = torch.eq(y_true, y_pred).sum().item()
+  return ((correct/len(y_true))*100)
 
 def precision_recall_f1Score(y_pred, y_true):
-    precision = metrics.precision_score(y_true, y_pred) # tp / (tp + fp)
-    recall = metrics.recall_score(y_true, y_pred) # tp / (tp + fn)
-    f1_score =  metrics.f1_score(y_true, y_pred) # 2 * (precision * recall) / (precision + recall)
-    return precision, recall, f1_score
+  precision = metrics.precision_score(y_true, y_pred) # tp / (tp + fp)
+  recall = metrics.recall_score(y_true, y_pred) # tp / (tp + fn)
+  f1_score =  metrics.f1_score(y_true, y_pred) # 2 * (precision * recall) / (precision + recall)
+  return precision, recall, f1_score
 
 def classification_report(y_pred, y_true):
-    report = metrics.classification_report(y_true, y_pred, *, labels=None, target_names=None, sample_weight=None, digits=2, output_dict=False, zero_division='warn')
-    return report
+  report = metrics.classification_report(y_true, y_pred, *, labels=None, target_names=None, sample_weight=None, digits=2, output_dict=False, zero_division='warn')
+  return report
